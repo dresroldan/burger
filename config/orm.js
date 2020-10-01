97075b1b96f5758fbd5bbc5953f314700c760dea
Andres Roldan <dresroldan@gmail.com>
2020-09-30 19:56:40 -0500

    basic setup

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+var connection = require("../config/connection.js");
+
+function selectAll();
+
+function insertOne();
+
+function updateOne();
+
+
+
+module.exports = orm;
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burgers;
+INSERT INTO burgers (burger_name, devoured) VALUES ('texas', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mcdouble', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('baconator', true);
+
